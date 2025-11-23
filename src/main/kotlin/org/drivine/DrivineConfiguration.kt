59187e5422d4c7cc8e1d32fb98c2aa7e4c3b4199
jasper blues <jasper@liberation-data.com>
2025-11-23 15:14:08 +1000

    Fix typo in component scan package name.

--- src/main/kotlin/org/drivine/DrivineConfiguration.kt
+++ src/main/kotlin/org/drivine/DrivineConfiguration.kt
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
-@ComponentScan(basePackages = ["drivine"])
+@ComponentScan(basePackages = ["org.drivine"])
 class DrivineConfiguration {
 
     @Bean
